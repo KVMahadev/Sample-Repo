a9aa05ae69c711eee669f87ddc0e45fca05e3ee0
KVMahadev <someshwarankrishna@gmail.com>
2024-04-29 17:17:16 +0530

    Tagging

---
+++ src/Programs/Tagging.java
@@ -0,0 +1,12 @@
+package Programs;
+
+public class Tagging {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is for checking github tagging");
+
+	}
+
+}
